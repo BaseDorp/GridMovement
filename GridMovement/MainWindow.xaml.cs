10f8d42c5a4dd3c330b6a0e547d6922cc037f7ec
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2018-11-11 02:43:04 -0600

    - Player is able to move - Doesn't move off screen - Works

--- GridMovement/MainWindow.xaml.cs
+++ GridMovement/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+// Big thanks to Jethro for helping me debug the grid movement
+// His github: https://github.com/JSchoppe
+
 namespace GridMovement
 {
     /// <summary>
@@ -28,23 +31,52 @@ namespace GridMovement
 
         private void bt_Right_Click(object sender, RoutedEventArgs e)
         {
-            Thickness _margin = img_Player.Margin;
-            img_Player.Margin = new Thickness(0, 0, 125, 0);
+            if (img_Player.Margin.Left - pixelsPerUnit <= 450)
+            {
+                Thickness _margin = img_Player.Margin;
+                img_Player.Margin = Translate(img_Player.Margin, new Vector(1, 0));
+            }
+        }
+        public static int pixelsPerUnit = 59;
+
+        public static Thickness Translate(Thickness toTranslate, Vector direction)
+        {
+            Thickness returnThickness = toTranslate;
+
+            // Update the left margin.
+            returnThickness.Left += direction.X * pixelsPerUnit;
+
+            // Subtraction because of the Ydown canvas.
+            returnThickness.Top -= direction.Y * pixelsPerUnit;
+
+            return returnThickness;
         }
 
         private void bt_Left_Click(object sender, RoutedEventArgs e)
         {
-            
+            if (img_Player.Margin.Left - pixelsPerUnit >= 0)
+            {
+                Thickness _margin = img_Player.Margin;
+                img_Player.Margin = Translate(img_Player.Margin, new Vector(-1, 0));
+            }
         }
 
         private void bt_Down_Click(object sender, RoutedEventArgs e)
         {
-
+            if (img_Player.Margin.Top - pixelsPerUnit <= 200)
+            {
+                Thickness _margin = img_Player.Margin;
+                img_Player.Margin = Translate(img_Player.Margin, new Vector(0, -1));
+            }
         }
 
         private void bt_Up_Click(object sender, RoutedEventArgs e)
         {
-
+            if (img_Player.Margin.Top - pixelsPerUnit >= 0)
+            {
+                Thickness _margin = img_Player.Margin;
+                img_Player.Margin = Translate(img_Player.Margin, new Vector(0, 1));
+            }
         }
     }
 }
